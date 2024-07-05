d293e8753b21e652f3734dee843424017ff81892
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-07-05 12:50:03 -0300

    feat: show CategoryItem for each Category

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -5,26 +5,35 @@ import { Category } from "../../types/category.types";
 import { env } from "process";
 
 import "./categories.styles.css";
+import CategoryItem from "../category-item/category-item.component";
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([]);
 
-  const fetchCategories = async () => {
-    try {
-      const { data } = await axios.get(`${env.apiUrl}/api/category`);
-      console.log({ data });
-    } catch (error) {
-      console.log({ error });
-    }
-  };
+  console.log(categories);
 
-  useEffect(() => {
-    fetchCategories();
-  }, []);
+  // const fetchCategories = async () => {
+  //   try {
+  //     const { data } = await axios.get(`${env.apiUrl}/api/category`);
+  //     console.log({ data });
+  //   } catch (error) {
+  //     console.log({ error });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchCategories();
+  // }, []);
 
   return (
     <div className="categories-container">
-      <div className="categories-content"></div>
+      <div className="categories-content">
+        <div>
+          {categories.map((category) => (
+            <CategoryItem category={category} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
