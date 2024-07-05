225e2545741114e22eaea706cd361d88319a8a5f
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-07-05 13:21:21 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: "#212529",
+  text: {
+    white: "#F8F9FA",
+    dark: "#343A40",
+  },
+  background: {
+    dark: "#212529",
+    white: "#F8F9FA",
+  },
+  input: {
+    background: "#E9ECEF",
+    placeholder: "#6c7570",
+  },
+  error: "#FF6A6A",
+};
+
+export default Colors;
