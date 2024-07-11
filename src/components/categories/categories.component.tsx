810ca618def21b473f16698eef166d96595106a6
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-07-11 15:36:24 -0300

    feat: get categories from firestore db

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,29 +1,37 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { getDocs, collection } from "firebase/firestore";
 
 import { Category } from "../../types/category.types";
-import { env } from "process";
 
 import CategoryItem from "../category-item/category-item.component";
 import { CategoriesContainer, CategoriesContent } from "./categories.styles";
+import { db } from "../../config/firebase.config";
+
+import { categoryConverter } from "../converters/firestore.converters";
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([]);
 
   console.log(categories);
 
-  // const fetchCategories = async () => {
-  //   try {
-  //     const { data } = await axios.get(`${env.apiUrl}/api/category`);
-  //     console.log({ data });
-  //   } catch (error) {
-  //     console.log({ error });
-  //   }
-  // };
+  const fetchCategories = async () => {
+    try {
+      const categoriesFromFirestore: Category[] = [];
+      const querySnapshot = await getDocs(
+        collection(db, "categories").withConverter(categoryConverter)
+      );
+      querySnapshot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data());
+      });
+      setCategories(categoriesFromFirestore);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchCategories();
-  // }, []);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
     <CategoriesContainer>
