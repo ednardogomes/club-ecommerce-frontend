9c4e938961fa4b6f10ede470b016761783d8303a
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-07-18 12:09:15 -0300

    feat: add Loading to Longin Page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -14,6 +14,7 @@ import CustomButton from "../../components/custom-button/custom-button.component
 import Header from "../../components/header/header.component";
 import CustomInput from "../../components/custom-input/custom-input.component";
 import InputErrorMessage from "../../components/input-error-message/input-error-message.component";
+import Loading from "../../components/loading/loading.component";
 
 import {
   LoginContainer,
@@ -26,7 +27,7 @@ import {
 import { auth, db, googleProvider } from "../../config/firebase.config";
 import { UserContext } from "../../contexts/user.context";
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface LoginForm {
@@ -42,6 +43,8 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<LoginForm>();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const { isAuthenticated } = useContext(UserContext);
   const navigate = useNavigate();
   useEffect(() => {
@@ -52,6 +55,7 @@ const LoginPage = () => {
 
   const handleSubmitPress = async (data: LoginForm) => {
     try {
+      setIsLoading(true);
       const userCredentials = await signInWithEmailAndPassword(
         auth,
         data.email,
@@ -59,17 +63,21 @@ const LoginPage = () => {
       );
     } catch (error) {
       const _error = error as AuthError;
-      console.log(error);
+
       if (_error.code === AuthErrorCodes.INVALID_IDP_RESPONSE) {
         setError("password", { type: "mismatch" });
         setError("email", { type: "mismatch" });
         return;
       }
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const handleSignInWithGooglePress = async () => {
     try {
+      setIsLoading(true);
+
       const userCredentials = await signInWithPopup(auth, googleProvider);
 
       const querySnapshot = await getDocs(
@@ -93,12 +101,15 @@ const LoginPage = () => {
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <>
       <Header />
+      {isLoading && <Loading />}
       <LoginContainer>
         <LoginContent>
           <LoginHeadline>Entre com a sua conta</LoginHeadline>
