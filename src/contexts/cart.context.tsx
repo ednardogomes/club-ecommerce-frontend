3c89cd4171db318fc46840d6c670077fffce67e4
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-07-19 15:44:20 -0300

    feat: ensure products can have their quantity decreased on cart

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -10,6 +10,7 @@ interface ICartContext {
   addProductToCart: (product: Product) => void;
   removeProductFromCart: (productId: string) => void;
   increaseProductQuantity: (productId: string) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 export const CartContext = createContext<ICartContext>({
   isVisible: false,
@@ -18,6 +19,7 @@ export const CartContext = createContext<ICartContext>({
   addProductToCart: () => {},
   removeProductFromCart: () => {},
   increaseProductQuantity: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 interface CartContextProviderProps {
@@ -68,6 +70,18 @@ const CartContextProvider: FunctionComponent<CartContextProviderProps> = ({
     );
   };
 
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((products) =>
+      products
+        .map((product) =>
+          product.id === productId
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+        )
+        .filter((product) => product.quantity > 0)
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
@@ -77,6 +91,7 @@ const CartContextProvider: FunctionComponent<CartContextProviderProps> = ({
         addProductToCart,
         removeProductFromCart,
         increaseProductQuantity,
+        decreaseProductQuantity,
       }}
     >
       {children}
