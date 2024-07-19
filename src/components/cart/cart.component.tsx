b2719e6fb8293610edc6544d58a110b8e189289a
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-07-19 16:20:29 -0300

    feat: add empty message to cart

--- src/components/cart/cart.component.tsx
+++ src/components/cart/cart.component.tsx
@@ -15,7 +15,7 @@ import { CartContext } from "../../contexts/cart.context";
 import CartItem from "../cart-item/cart-item.component";
 
 const Cart: FunctionComponent = () => {
-  const { isVisible, products, toggleCart, productsTotalPrice } =
+  const { isVisible, products, toggleCart, productsTotalPrice, productsCount } =
     useContext(CartContext);
 
   return (
@@ -23,13 +23,22 @@ const Cart: FunctionComponent = () => {
       <CartEscapeArea onClick={toggleCart} />
       <CartContent>
         <CartTitle>Seu Carrinho</CartTitle>
+
         {products.map((product) => (
           <CartItem key={product.id} product={product} />
         ))}
-        <CartTotal>Total: R${productsTotalPrice}</CartTotal>
-        <CustomButton startIcon={<BsCartCheck></BsCartCheck>}>
-          Ir para o Checkout
-        </CustomButton>
+
+        {productsCount > 0 && (
+          <CartTotal>Total: R${productsTotalPrice}</CartTotal>
+        )}
+
+        {productsCount > 0 && (
+          <CustomButton startIcon={<BsCartCheck></BsCartCheck>}>
+            Ir para o Checkout
+          </CustomButton>
+        )}
+
+        {productsCount === 0 && <p>Seu carrinho está vazio!</p>}
       </CartContent>
     </CartContainer>
   );
